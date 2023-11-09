ae2afe26ddc162d758d7a2768a537544ff7281d8
CodeWithSubaru <francis.cloydm@gmail.com>
2023-11-09 12:53:10 +0800

    Disable feature for future used

---
+++ setPosition.ts
@@ -0,0 +1,15 @@
+// const handleGetPosition = (position: any) =>
+//   setUserPosition({
+//     ...userPosition,
+//     lat: position.coords.latitude,
+//     long: position.coords.longitude,
+//   });
+
+// useEffect(() => {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(handleGetPosition);
+//   }
+// }, []);
+
+// const timestamp = Date.now();
+// const formattedTime = formatTimeTo12Hour(timestamp);
